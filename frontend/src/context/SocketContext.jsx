12458a97cf2a222362eba9d665febfcd0231e748
MdMizanur0 <mdmizanur5773@gmail.com>
2024-04-17 00:03:08 +0530

    baby

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://talk-hub-j24s.onrender.com/", {
+			const socket = io("https://talkhub-m1cc.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
